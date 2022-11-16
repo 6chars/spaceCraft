8f5d374f83535eb07e31028d76645446eaa1d33e
6chars <105956969+6chars@users.noreply.github.com>
2022-11-15 22:57:38 -0600

    Update spaceCraft.py

--- spaceCraft/spaceCraft.py
+++ spaceCraft/spaceCraft.py
@@ -8,7 +8,10 @@ global rotate
 global x
 global y
 global shiphealth
+global shipfuel
+global fueldrain
 global hitdelay
+global section
 k = 0
 i = 0
 x = 500
@@ -27,6 +30,7 @@ shipPos = [500,500]
 squaresX = []
 squaresY = []
 starStatus = []
+terrains = []
 fired = []
 delay10 = 10
 seekerdelay = []
@@ -44,6 +48,8 @@ engineheat = 0
 shipDcelltime = 0
 shipDcellspeed = -1
 shiphealth = 3
+shipfuel = 10000
+fueldrain = 3
 hitdelay = 10
 for i in range(enemyX.__len__()):
     randMovX.append(0)
@@ -55,6 +61,8 @@ for i in range(400):
     squaresX.append(random.randrange(-50000,50000))
     squaresY.append(random.randrange(-50000,50000))
     starStatus.append('white')
+    terrains.append([[],[],[],[],[]])
+
 while True:
     for i in [pygame.time.delay(10)]:
         screen.fill('black')
@@ -88,7 +96,8 @@ while True:
                     shipDcelltime = 0
                     shipbinaries[1] = 0
                     engineheat = 0
-        healthDisplay = screen.blit(text.render('Ship Health: '+ str(shiphealth),True,'White'),(800,900))#         <---------------- ^Ship state measurement and handling^ ----------------
+        healthDisplay = screen.blit(text.render('Ship Health: '+ str(shiphealth),True,'White'),(800,900))
+        fuelDisplay = screen.blit(text.render('Fuel: '+ str(shipfuel),True,'White'),(800,950))#         <---------------- ^Ship state measurement and handling^ ----------------
         if rotate >= 360:
             rotate = 0
         elif rotate <= 0:
@@ -117,6 +126,7 @@ while True:
                 x -= rotate / 10
             point[0] = x-point[0]
             point[1] = y-point[1]
+            shipfuel -= fueldrain
         else:
             shipbinaries[0] = 0
         if key[pygame.K_m]:
@@ -197,11 +207,55 @@ while True:
             if shipbinaries[2]: 
                 if (squaresX[i]/10)-x/10 < 1800 and (squaresY[i]/10)-y/10 < 1000:
                     screen.fill(starStatus[i],(((squaresX[i]/10)-x/10)+800,((squaresY[i]/10)-y/10)+500,2,2))                
-            if squaresX[i]-x < 1800 and squaresY[i]-y < 1000 and squaresX[i]-x > 0 and squaresY[i]-y > 0:              
-                screen.fill('blue',(squaresX[i]-x,squaresY[i]-y,20,20))
-                starStatus[i] = 'green'
-            i+=1#                                                       <------^star rendering^------
+            if squaresX[i]-x < 1800 and squaresY[i]-y < 1000 and squaresX[i]-x > 0 and squaresY[i]-y > 0:
+                if abs(squaresX[i]-x) < 25 and abs(squaresY[i]-y) < 25:
+                    global xp
+                    xp = 0
+                    section += 20
+                    while True:
+                        for i in [pygame.time.delay(1)]:
+                            screen.fill('black')
+                            pygame.event.get()
+                            key = pygame.key.get_pressed()
+                            if key[pygame.K_a]:
+                                xp -= 1
+                            if key[pygame.K_d]:
+                                xp += 1
+                            for p in range(200):
+                                if terrains[i][4][p] == True:
+                                    screen.fill('grey',(i*50-xp,950,50,50))   
+                                if terrains[i][3][p] == True:
+                                    screen.fill('grey',(i*50-xp,900,50,50))          
+                                if terrains[i][2][p] == True:
+                                    screen.fill('grey',(i*50-xp,850,50,50))    
+                                if terrains[i][1][p] == True:
+                                    screen.fill('grey',(i*50-xp,800,50,50))  
+                                if terrains[i][0][p] == True:
+                                    screen.fill('grey',(i*50-xp,750,50,50)) 
+                else:
+                    screen.fill('blue',(squaresX[i]-x,squaresY[i]-y,20,20))
+                    if starStatus == 'white':
+                        section = 0
+                        for i in range(200):
+                            terrains[i][0].append(False) #a
+                            terrains[i][1].append(False) #b
+                            terrains[i][2].append(False) #c
+                            terrains[i][3].append(False) #d
+                            terrains[i][4].append(True) #e
+                        for i in range(10):
+                            dPush = random.randrange(0,6)
+                            dWide = random.randrange(0,16)
+                            for i in range(dWide):
+                                terrains[i][3][dPush+i+section] = True
+                                if random.randrange(0,6) > 2:
+                                    terrains[i][2][dPush+i+section] = True
+                                    if random.randrange(0,6) > 2:
+                                        terrains[i][1][dPush+i+section] = True
+                                        if random.randrange(0,6) > 2:
+                                            terrains[i][0][dPush+i+section] = True
+                        starStatus[i] = 'green'
+            i+=1#                                                       <------^astral rendering^------
         pygame.display.update()
 #features: Hitpoints, first unlock, capacities,  destructible or physical objects, upgradeable unlocks, better sprites, particles, sounds, trading posts & credits
 #bugs: point not defined on start and turn, 30 projectiles simulated indefinately, static projectile image
-#unlocks: energy capacity, map unlock, rechargeable shield, timed explosive, plasma beam, warp drive, limited missiles,
+#unlocks: terrain scanner, energy capacity, map unlock, rechargeable shield, timed explosive, plasma beam, warp drive, limited missiles,
